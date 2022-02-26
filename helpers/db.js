2476f4944ae38ae2a66382217e644c64162eed56
theissn <5305773+theissn@users.noreply.github.com>
2022-02-26 19:52:40 +0000

    wip

--- helpers/db.js
+++ helpers/db.js
@@ -3,4 +3,5 @@ import Dexie from 'dexie';
 export const db = new Dexie('myDatabase');
 db.version(1).stores({
   exercises: '++id, name',
+  workouts: '++id, name, date, exercises',
 });
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav className="flex content-between fixed w-full bottom-0 text-center">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
